432f4dc70078761279bb170e7bf93713d22c9a62
andrysiaczek <agata.and.11@gmail.com>
2020-12-17 00:59:40 +0100

    add calculate area methods

--- src/main/java/pl/put/poznan/info/logic/BuildingInfo.java
+++ src/main/java/pl/put/poznan/info/logic/BuildingInfo.java
@@ -14,8 +14,37 @@ public class BuildingInfo {
         this.building = new Gson().fromJson(info, Building.class);
     }
 
-    public String inform(String text){
-        // of course, normally it would do something based on the info
-        return text.toUpperCase();
+    public String calculateBuildingArea(){
+        int area = 0;
+        for (Level l : building.getLevels()) {
+            for (Room r: l.getRooms()) {
+                area += r.getArea();
+            }
+        }
+        return String.valueOf(area);
+    }
+
+    public String calculateLevelArea(String id) {
+        int area = 0;
+        for (Level l : building.getLevels()) {
+            if (l.getId().equals(id)){
+                for (Room r: l.getRooms()) {
+                    area += r.getArea();
+                }
+                return String.valueOf(area);
+            }
+        }
+        return "No level with the given id.";
+    }
+
+    public String calculateRoomArea(String id) {
+        for (Level l : building.getLevels()) {
+            for (Room r: l.getRooms()) {
+                if (r.getId().equals(id)) {
+                    return String.valueOf(r.getArea());
+                }
+            }
+        }
+        return "No room with the given id.";
     }
 }
