d2ef3fcfb9199efe75b21c092ddbaa4edc6e67a8
andrysiaczek <agata.and.11@gmail.com>
2021-01-18 18:06:50 +0100

    add BuildingInfoAreaController.java info

--- src/main/java/pl/put/poznan/info/rest/BuildingInfoAreaController.java
+++ src/main/java/pl/put/poznan/info/rest/BuildingInfoAreaController.java
@@ -1,16 +1,27 @@
 package pl.put.poznan.info.rest;
+
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.info.logic.BuildingInfo;
 
-
+/**
+ * This is the BuildingInfoAreaController class, which contains endpoints returning data about the building area.
+ */
 @RestController
 public class BuildingInfoAreaController {
-
+    /**
+     * the logger attribute logging info about app execution at the DEBUG and INFO levels
+     */
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoAreaController.class);
 
+    /**
+     * Endpoint finding total area of the building.
+     *
+     * @param buildingData the building data in json format
+     * @return area of the building
+     */
     @RequestMapping(value = "area/building", method = RequestMethod.POST, produces = "application/json")
     public String buildingArea(@RequestBody String buildingData) throws JSONException {
 
@@ -22,6 +33,13 @@ public class BuildingInfoAreaController {
         return area;
     }
 
+    /**
+     * Endpoint finding area of the level specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required level
+     * @return area of the level specified by id
+     */
     @RequestMapping(value = "area/level/{id}", method = RequestMethod.POST, produces = "application/json")
     public String levelArea(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -35,6 +53,13 @@ public class BuildingInfoAreaController {
         return area;
     }
 
+    /**
+     * Endpoint finding area of the room specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required room
+     * @return area of the room specified by id
+     */
     @RequestMapping(value = "area/room/{id}", method = RequestMethod.POST, produces = "application/json")
     public String roomArea(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -47,6 +72,13 @@ public class BuildingInfoAreaController {
         return area;
     }
 
+    /**
+     * Endpoint finding rooms with area smaller or equal to specified limit level.
+     *
+     * @param buildingData the building data in json format
+     * @param level the specified area limit level
+     * @return ids of rooms within limited area level
+     */
     @RequestMapping(value = "area/limit/{level}", method = RequestMethod.POST, produces = "application/json")
     public String limitedArea(@RequestBody String buildingData, @PathVariable String level) throws JSONException {
 
