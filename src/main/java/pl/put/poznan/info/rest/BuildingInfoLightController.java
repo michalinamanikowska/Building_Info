9e068360a39ef8040d692f57ac3c734e6764f178
andrysiaczek <agata.and.11@gmail.com>
2021-01-18 18:19:23 +0100

    add BuildingInfoLightController.java info

--- src/main/java/pl/put/poznan/info/rest/BuildingInfoLightController.java
+++ src/main/java/pl/put/poznan/info/rest/BuildingInfoLightController.java
@@ -6,11 +6,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.info.logic.BuildingInfo;
 
+/**
+ * This is the BuildingInfoLightController class, which contains endpoints returning data about the building light.
+ */
 @RestController
 public class BuildingInfoLightController {
-
+    /**
+     * the logger attribute which logs light info about app execution at the DEBUG and INFO levels
+     */
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoLightController.class);
 
+    /**
+     * Endpoint finding total light of the building.
+     *
+     * @param buildingData the building data in json format
+     * @return light of the building
+     */
     @RequestMapping(value = "light/building", method = RequestMethod.POST, produces = "application/json")
     public String buildingLight(@RequestBody String buildingData) throws JSONException {
 
@@ -22,6 +33,13 @@ public class BuildingInfoLightController {
         return light;
     }
 
+    /**
+     * Endpoint finding light of the level specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required level
+     * @return light of the level specified by id
+     */
     @RequestMapping(value = "light/level/{id}", method = RequestMethod.POST, produces = "application/json")
     public String levelLight(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -34,6 +52,13 @@ public class BuildingInfoLightController {
         return light;
     }
 
+    /**
+     * Endpoint finding light of the room specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required room
+     * @return light of the room specified by id
+     */
     @RequestMapping(value = "light/room/{id}", method = RequestMethod.POST, produces = "application/json")
     public String roomLight(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -46,6 +71,13 @@ public class BuildingInfoLightController {
         return light;
     }
 
+    /**
+     * Endpoint finding rooms with light smaller or equal to specified limit level.
+     *
+     * @param buildingData the building data in json format
+     * @param level the specified light limit level
+     * @return ids of rooms within limited light level
+     */
     @RequestMapping(value = "light/limit/{level}", method = RequestMethod.POST, produces = "application/json")
     public String limitedLight(@RequestBody String buildingData, @PathVariable String level) throws JSONException {
 
