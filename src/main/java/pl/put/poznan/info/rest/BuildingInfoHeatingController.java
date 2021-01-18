0d29c29c296e428f8ee3b3bd0041b53703f57a8e
andrysiaczek <agata.and.11@gmail.com>
2021-01-18 18:15:39 +0100

    add BuildingInfoHeatingController.java info

--- src/main/java/pl/put/poznan/info/rest/BuildingInfoHeatingController.java
+++ src/main/java/pl/put/poznan/info/rest/BuildingInfoHeatingController.java
@@ -1,16 +1,27 @@
 package pl.put.poznan.info.rest;
+
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.info.logic.BuildingInfo;
 
-
+/**
+ * This is the BuildingInfoHeatingController class, which contains endpoints returning data about the building heating.
+ */
 @RestController
 public class BuildingInfoHeatingController {
-
+    /**
+     * the logger attribute which logs heating info about app execution at the DEBUG and INFO levels
+     */
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoHeatingController.class);
 
+    /**
+     * Endpoint finding total heating of the building.
+     *
+     * @param buildingData the building data in json format
+     * @return heating of the building
+     */
     @RequestMapping(value = "heating/building", method = RequestMethod.POST, produces = "application/json")
     public String buildingHeating(@RequestBody String buildingData) throws JSONException {
 
@@ -22,6 +33,13 @@ public class BuildingInfoHeatingController {
         return heating;
     }
 
+    /**
+     * Endpoint finding heating of the level specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required level
+     * @return heating of the level specified by id
+     */
     @RequestMapping(value = "heating/level/{id}", method = RequestMethod.POST, produces = "application/json")
     public String levelHeating(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -34,6 +52,13 @@ public class BuildingInfoHeatingController {
         return heating;
     }
 
+    /**
+     * Endpoint finding heating of the room specified by id.
+     *
+     * @param buildingData the building data in json format
+     * @param id the identifier of the required room
+     * @return heating of the room specified by id
+     */
     @RequestMapping(value = "heating/room/{id}", method = RequestMethod.POST, produces = "application/json")
     public String roomHeating(@RequestBody String buildingData, @PathVariable String id) throws JSONException {
 
@@ -46,6 +71,13 @@ public class BuildingInfoHeatingController {
         return heating;
     }
 
+    /**
+     * Endpoint finding rooms with heating greater than specified level.
+     *
+     * @param buildingData the building data in json format
+     * @param level the specified heating level
+     * @return ids of rooms exceeding specified heating level
+     */
     @RequestMapping(value = "heating/exceed/{level}", method = RequestMethod.POST, produces = "application/json")
     public String exceededHeating(@RequestBody String buildingData, @PathVariable String level) throws JSONException {
 
@@ -59,6 +91,13 @@ public class BuildingInfoHeatingController {
         return rooms;
     }
 
+    /**
+     * Endpoint finding rooms with heating smaller or equal to specified limit level.
+     *
+     * @param buildingData the building data in json format
+     * @param level the specified heating limit level
+     * @return ids of rooms within limited heating level
+     */
     @RequestMapping(value = "heating/limit/{level}", method = RequestMethod.POST, produces = "application/json")
     public String limitedHeating(@RequestBody String buildingData, @PathVariable String level) throws JSONException {
 
